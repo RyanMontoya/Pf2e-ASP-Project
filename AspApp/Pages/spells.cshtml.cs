b8fbf0c39c15034f1791cf013e180cded1d49ce3
ryan-montoya <rmonto6@uw.edu>
2023-03-03 23:25:50 -0800

    A bit of clean up before API research.

---
+++ AspApp/Pages/spells.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspApp.Pages
+{
+    public class spellsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AspApp/Pages/tags.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspApp.Pages
+{
+    public class tagsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
