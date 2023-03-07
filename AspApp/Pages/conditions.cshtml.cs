0ccee3eabffe05e3f14e7402afdbfe0d51be583e
ryan-montoya <rmonto6@uw.edu>
2023-03-06 20:16:45 -0800

    added a page.

---
+++ AspApp/Pages/conditions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspApp.Pages
+{
+    public class conditionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
